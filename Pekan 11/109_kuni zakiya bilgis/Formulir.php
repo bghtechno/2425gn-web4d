f2ec44b50f9ffe820e3d39792dc8a79d94bb3032
Zakiya07 <zakiyabilgis@gmail.com>
2025-06-09 06:48:27 -0700

    Update Formulir.php

--- Pekan 11/109_kuni zakiya bilgis/Formulir.php
+++ Pekan 11/109_kuni zakiya bilgis/Formulir.php
@@ -1,1 +1,31 @@
+<?php
+$submitted = $_SERVER["REQUEST_METHOD"] == "POST";
+$name = $email = "";
+
+if ($submitted) {
+  $name = htmlspecialchars($_POST["name"]);
+  $email = htmlspecialchars($_POST["email"]);
+}
+?>
+
+<!DOCTYPE html>
+<html>
+<body>
+
+<h2>Formulir Pendaftaran</h2>
+
+<form method="POST">
+  Name: <input type="text" name="name"><br><br>
+  E-mail: <input type="text" name="email"><br><br>
+  <input type="submit">
+</form>
+
+<?php if ($submitted): ?>
+  <h3>Data yang Anda Kirim:</h3>
+  Name: <?php echo $name; ?><br>
+  Email: <?php echo $email; ?>
+<?php endif; ?>
+
+</body>
+</html>
 
